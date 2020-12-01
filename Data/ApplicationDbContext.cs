b54ead605ad94679fdc8a47ab7766c372d57fe36
Murilo A <muriloandradealmeida@gmail.com>
2020-12-01 21:39:06 +0000

    Project and initial setup added

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using dockerized_dotnet_angular.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dockerized_dotnet_angular.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
